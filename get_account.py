585aa801a9e2e6fcf51d23bf586ccc2f534a5235
jesuscirino <jesuscirino@gmail.com>
2022-04-07 21:30:28 -0500

    pandas comprendido

--- get_account.py
+++ get_account.py
@@ -7,7 +7,10 @@ from config import BASE_URL as base
 
 from rich.console import Console
 from rich.logging import RichHandler
+from rich.traceback import install
+install()
 
+import pandas as pd
 def debugit(msg):
 
     logging.basicConfig(
@@ -19,9 +22,18 @@ def debugit(msg):
     #logging.info(msg)
     logging.debug(msg)
 
-con = Console()
+c = Console()
 
-client = Client(key, secret, base_url=base)
-#logging.info(client.account(recvWindow=6000))
-con.log(client.account(recvWindow=6000))
-#debugit(client.account(recvWindow=6000))
+
+
+if __name__ == "__main__":
+    client = Client(key, secret, base_url=base)
+    account_info = client.account(recvWindow=6000)
+    df_balances = pd.DataFrame(account_info['balances'])
+    df_balances['free'] = pd.to_numeric(df_balances['free'],errors='coerce').fillna(0)
+    df_balances['locked'] = pd.to_numeric(df_balances['free'],errors='coerce').fillna(0)
+    c.print(account_info.keys())
+    c.print(df_balances.info())
+    c.print(df_balances[df_balances['free']>0])
+    #c.print(df_balances[df_balances['asset']=='XRP'])
+    del df_balances
